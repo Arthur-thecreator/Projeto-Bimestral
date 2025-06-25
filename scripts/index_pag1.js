03cc2bf3b97b3c29b75430737236624de1d218cc
Arthur-thecreator <arthur.mendes@aluno.ifsp.edu.br>
2025-06-24 22:51:47 -0300

    primeira pag feita

---
+++ scripts/index_pag1.js
@@ -0,0 +1,20 @@
+const body = document.querySelector("body")
+
+const infoUser = document.createElement("div")
+    const sintoma_obs = document.createElement("section")
+        const selectSintomas = document.createElement("selector")
+            const legendaSelectSintomas = document.createElement("label")            
+        const caixaObs = document.createElement("textarea")
+    
+    const areaEndereco = document.createElement("section")
+        const rua = document.createElement("textarea")
+        const num = document.createElement("textarea")
+        const bairro = document.createElement("textarea")
+        const cidade = document.createElement("textarea")
+        const botaoLocalizacao = document.createElement("button")
+
+
+
+const mapa = document.createElement("section")
+
+const diagButton = document.createElement("button")
