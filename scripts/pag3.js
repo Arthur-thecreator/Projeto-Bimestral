9e53ff650e9c2a7a8a478ea364ba264a37e923a0
Arthur-thecreator <arthur.mendes@aluno.ifsp.edu.br>
2025-06-25 22:28:41 -0300

    tudo antes de modificar pag3

--- scripts/pag3.js
+++ scripts/pag3.js
@@ -1,8 +1,11 @@
 const body = document.querySelector("body")
 
+
 const areaImagem = document.createElement("div")
+areaImagem.id = "areaImagem"
 
 const areaInfoPaciente = document.createElement("div")
+areaInfoPaciente.id = "areaInfopaciente"
 
 const nome = document.createElement("textarea")
 nome.placeholder = "nome"
@@ -22,10 +25,12 @@ verRecomendacoes.id = "verRecomendações"
 const legendaAreaInfoPaciente = document.createElement("label")
 legendaAreaInfoPaciente.htmlFor = "area de informações do paciente"
 legendaAreaInfoPaciente.innerText = "Dados do paciente"
+legendaAreaInfoPaciente.className = "legendas"
 
 const legendaVerOpcoes = document.createElement("label")
 legendaVerOpcoes.htmlFor = "area de recomedacoes"
 legendaVerOpcoes.innerText = "ver recomendações da IA"
+legendaVerOpcoes.className = "legendas"
 
 body.append(areaImagem, areaInfoPaciente)
 
